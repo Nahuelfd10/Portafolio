0b509fb0d2c50cd23a136b64f34ca43b90dd8932
NahuelFD <nahuelfd10@gmail.com>
2023-07-16 20:57:50 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <>
+      <h1>ASDASDASDASD</h1>
       <Header />
       <Nav />
       <About />
@@ -22,7 +23,7 @@ const App = () => {
       <Contact />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
